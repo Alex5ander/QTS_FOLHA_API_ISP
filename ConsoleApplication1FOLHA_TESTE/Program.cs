84b282387056d7f2f7f247349b173f9f52ef3628
Alexsander <alexsandergutierreez@gmail.com>
2022-06-03 22:23:47 -0300

    first

---
+++ ConsoleApplication1FOLHA_TESTE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1FOLHA_TESTE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ FOLHA_API_ISP/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOLHA_API_ISP
+{
+    public class Funcionario : IImposto, ISalario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; };
+        public double SB { get; set; }
+        public double SL { get; set; }
+        public Funcionario(int Codigo, string Nome, double SB)
+        {  
+            this.Codigo = Codigo;
+            this.Nome = Nome;
+            this.SB = SB;
+        }
+
+        public void Calcular()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Folha()
+        {
+            throw new NotImplementedException();
+        }
+
+        double IImposto.INSS()
+        {
+            throw new NotImplementedException();
+        }
+
+        double IImposto.IRRF()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
