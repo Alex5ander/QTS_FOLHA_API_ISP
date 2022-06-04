de37df8e1096b4b0b57cb70561f20ee672201a78
Alexsander <alexsandergutierreez@gmail.com>
2022-06-03 22:38:27 -0300

    first

---
+++ FOLHA_TESTE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FOLHA_API_ISP;
+
+namespace FOLHA_TESTE
+{
+    class Program
+    {
+        static List<Funcionario> Empregados;
+
+        static void Main(string[] args)
+        {
+            
+            Empregados = new List<Funcionario>();
+            Empregados.Add(new Funcionario(1, "JOAO", 2300));
+            Empregados.Add(new Funcionario(2, "MARIA", 2700));
+            foreach (Funcionario f in Empregados)
+            {
+                f.Folha();
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
