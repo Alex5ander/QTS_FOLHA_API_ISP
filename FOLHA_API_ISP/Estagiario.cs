7b82f36c13fdfa0e0beea18c24944418de61c553
Alexsander <alexsandergutierreez@gmail.com>
2022-06-09 21:25:33 -0300

    aula

---
+++ FOLHA_API_ISP/Estagiario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOLHA_API_ISP
+{
+    class Estagiario:IEstagiario
+    {
+        public void Ponto(double horas, double SalHora)
+        {
+            
+        }
+
+        public void Imprimir()
+        {
+           
+        }
+
+        public void Calcular()
+        {
+            
+        }
+
+        public void Folha()
+        {
+            
+        }
+    }
+}
---
+++ FOLHA_API_ISP/IEstagiario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOLHA_API_ISP
+{
+    public interface IEstagiario: ISalario
+    {
+        void Ponto(double horas, double SalHora);
+        void Imprimir();
+    }
+}
